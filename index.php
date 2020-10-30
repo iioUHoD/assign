5dd66e036df39ada2fbeb29bfb3f24ffbd05e41a
iioUHoD <41914088+iioUHoD@users.noreply.github.com>
2020-10-30 12:41:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
     <title>Test</title>
 </head>
 <body>
-<?php
-echo "<h2>Chinatip Chanpong</h2>";
-echo "Pae<br>";
-echo <img src="cat.jpg">;
-echo <img src="cat2.jpg">;
-?>
+    <?php
+        echo "<h2>Chinatip Chanpong</h2>";
+        echo "Pae<br>";
+        echo <img src="cat.jpg">;
+        echo <img src="cat2.jpg">;
+    ?>
+    echo <a href="http://www.it.kmitl.ac.th">This is a Link</a>;
 </body>
 </html>
