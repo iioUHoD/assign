3f0f4447f8add8a4ea90c28989e6d12b02240d81
iioUHoD <41914088+iioUHoD@users.noreply.github.com>
2020-10-30 12:39:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <html>
-    <head>
-        <title>Hello World</title>
-    </head>
-    <body>
+<head>
+    <title>Test</title>
+</head>
+<body>
 <?php
 echo "<h2>Chinatip Chanpong</h2>";
 echo "Pae<br>";
 echo <img src="cat.jpg">;
 echo <img src="cat2.jpg">;
 ?>
-    </body>
+</body>
 </html>
