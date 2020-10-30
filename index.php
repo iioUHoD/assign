d12b0921e50be44ef4c60302c3b5bb0f14843642
iioUHoD <41914088+iioUHoD@users.noreply.github.com>
2020-10-30 12:50:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 </head>
 <body>
     <?php
-        echo "<h2>Chinatip Chanpong</h2>";
-        echo "Pae<br>";
+        echo "<h2>Chinatip Chanpong Pae</h2>";
+        echo "Wife to husband: Darling, doctor suggested me to go to Switzerland or Paris for relaxation, where shall we go?<br>";
+        echo "Husband: Other doctor!<br>";
         echo "<img src=\"cat.jpg\">";
         echo "<img src=\"cat2.jpg\">";
     ?>
