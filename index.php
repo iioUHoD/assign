7a6843127799313623aaebbf4e6141ff25a303d2
iioUHoD <41914088+iioUHoD@users.noreply.github.com>
2020-10-30 12:47:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <?php
         echo "<h2>Chinatip Chanpong</h2>";
         echo "Pae<br>";
-        echo <img src="cat.jpg">;
-        echo <img src="cat2.jpg">;
+        echo "<img src=\"cat.jpg\">";
+        echo "<img src=\"cat2.jpg\">";
     ?>
     <a href="http://www.it.kmitl.ac.th">This is a Link</a>;
 </body>
