8f940a2072afbfbb292964349152ffac0341389e
iioUHoD <41914088+iioUHoD@users.noreply.github.com>
2020-10-30 12:15:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
-<html>
-    <head>
-        <title>Hello World</title>
-    </head>
-    <body>
-        <h1>My first heading</h1>
-        <p>My first paragraph</p>
-        <img src="cat.jpg">
-        <br>
-        <a href="http://www.it.kmitl.ac.th">This is a Link</a>
-
-        <h2>The unordered list</h2>
-        <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
-        </ul>
-
-    </body>
-</html>
+<?php
+echo "<h2PHP is Fun!</h2>";
+echo "Hello World!<br>";
+?>
